5caacb83cd6bc06749696a05633a5a55631fc030
GusFors <gf222cy@student.lnu.se>
2021-09-20 22:18:19 +0200

    testing new discord api for joining voice channels

--- BombaBot.js
+++ BombaBot.js
@@ -1,17 +1,31 @@
 'use strict'
+
 const { Client, Intents } = require('discord.js')
+const {
+  generateDependencyReport,
+  AudioPlayerStatus,
+  AudioResource,
+  entersState,
+  joinVoiceChannel,
+  VoiceConnection,
+  VoiceConnectionStatus,
+} = require('@discordjs/voice')
+
 const EventHandler = require('./EventHandler')
 
 class BombaBot {
   client
   eventHandler
+  voiceChannelConnection
 
   constructor() {
     console.log('I have been created!!!!')
   }
 
   login() {
-    this.client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+    this.client = new Client({
+      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS],
+    })
     this.client.login(process.env.TOKEN)
   }
 
@@ -21,7 +35,22 @@ class BombaBot {
   }
 
   initEventHandler() {
-      this.eventHandler = new EventHandler(this.client)
+    this.eventHandler = new EventHandler(this.client)
+    this.eventHandler.addSubscriber(this)
+    // this.client.on('messageCreate', this.eventHandler.onMessage)
+  }
+
+  async joinChannel(channelDetails) {
+    //console.log('trying to join ' + channel)
+    // joinVoiceChannel()
+    this.voiceChannelConnection = joinVoiceChannel({
+      channelId: channelDetails.id,
+      guildId: channelDetails.guild.id,
+      adapterCreator: channelDetails.guild.voiceAdapterCreator,
+      selfDeaf: false,
+      selfMute: false,
+    })
+    console.log(this.voiceChannelConnection)
   }
 }
 
@@ -35,10 +64,10 @@ class BombaBot {
 
 //   init: () => {
 //     this.client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
-//    
+//
 //       console.log(this)
 //       login(process.env.TOKEN)
-//     
+//
 //   },
 // }
 
--- EventHandler.js
+++ EventHandler.js
@@ -1,24 +1,56 @@
-'use strict'
+const prefix = '!b'
 
 class EventHandler {
   client
+  subscribers
 
-  constructor(client) {
+  constructor(client, options) {
     console.log('EventHandler created')
     this.client = client
     this.startEventListener()
+    this.setOptions()
+    this.subscribers = []
+  }
+
+  addSubscriber(subscriber) {
+    this.subscribers.push(subscriber)
   }
 
   startEventListener() {
-    this.client.on('messageCreate', this.onMessage)
+    this.client.on('messageCreate', this.onMessage.bind(this))
   }
 
-  async onMessage(message) {
+  setOptions(optionsObject) {}
+
+  onMessage(message) {
     if (message.author.bot) return
 
-    console.log(message.cleanContent)
-    message.reply('testing reply')
-    console.log('ää')
+    // console.log(message.cleanContent)
+
+    if (!message.cleanContent.startsWith(prefix)) {
+      console.log('missing prefix')
+      return
+    }
+
+    let cleanCommand = message.cleanContent.split(' ')[1].trim() ? message.cleanContent.split(' ')[1].trim() : false
+    console.log(message.member.voice.channel)
+
+    if (cleanCommand === 'join') {
+      this.subscribers.forEach((sub) => {
+        console.log('should join')
+        //console.log(message.member.voice.channel)
+        sub.joinChannel(message.member.voice.channel)
+
+        //sub.joinChannel(await message.member.voice.channel)
+      })
+
+      // connection = await message.member.voice.channel.join()
+      // isConnectedToVoiceChannel = true
+      // dispatcher = connection.play('./audioFiles/vbrazil.mp3')
+      // dispatcher.setVolume(currentVolume)
+    }
+
+    //message.reply('0w0')
   }
 }
 
