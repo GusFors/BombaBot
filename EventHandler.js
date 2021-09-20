54c87384da142a123c925d1a952d94d672353e7d
GusFors <gf222cy@student.lnu.se>
2021-09-20 19:24:37 +0200

    testing very basic message reply

--- EventHandler.js
+++ EventHandler.js
@@ -6,6 +6,19 @@ class EventHandler {
   constructor(client) {
     console.log('EventHandler created')
     this.client = client
+    this.startEventListener()
+  }
+
+  startEventListener() {
+    this.client.on('messageCreate', this.onMessage)
+  }
+
+  async onMessage(message) {
+    if (message.author.bot) return
+
+    console.log(message.cleanContent)
+    message.reply('testing reply')
+    console.log('ää')
   }
 }
 
